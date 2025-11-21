786fbff47abcee1cd3e8d6ebe97fd821ce9697ae
Cs12krisz <csigerkrisztian@gmail.com>
2025-11-21 07:50:51 +0100

    Packages hozzáadva

--- BlogAPI/obj/Debug/net8.0/BlogAPI.AssemblyInfo.cs
+++ BlogAPI/obj/Debug/net8.0/BlogAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9525d6a4df483183621706ee63e3b5f866af3352")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
